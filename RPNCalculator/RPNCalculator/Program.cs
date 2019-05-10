f5d9b5a0cc90aa98a4f969d84dd12a8aed124d9f
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-10 11:44:49 +0200

    Init

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -6,6 +6,27 @@ namespace RPNCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, this is RPN calculator");
+            string expression = "2 3 +";
+            double expectedOutput = 5;
+            double output = PostfixEvaluator(expression);
+            Console.WriteLine("Expression: {0}", expression);
+            Console.WriteLine("Expected output: {0}", expectedOutput);
+            Console.WriteLine("Output: {0}", output);
+
+        }
+        static double PostfixEvaluator(string expression)
+        {
+            return 0;
+        }
+    }
+
+    class Stack<T>
+    {
+        int count;
+        Stack()
+        {
+            count = 0;
         }
     }
 }
