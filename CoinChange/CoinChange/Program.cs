f482fef8792bcb421199712d46fdbadb1a09c0bf
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-17 11:55:52 +0200

    Simple greedy change

--- CoinChange/CoinChange/Program.cs
+++ CoinChange/CoinChange/Program.cs
@@ -11,8 +11,20 @@ namespace CoinChange
             Console.WriteLine("Hello, this is coin change calculator");
             Console.WriteLine("Available coins: {0}", string.Join(", ", Coins));
             Console.WriteLine("Change to be made: {0}", change);
-            
+            make_change(Coins, change);
             
         }
+        public static void make_change(int[]coins,int change)
+        {
+            Array.Sort(coins);
+            Array.Reverse(coins);
+
+            foreach(int coin in coins)
+            {
+                int how_many = change / coin;
+                change = change % coin;
+                Console.WriteLine(how_many+"x"+coin);
+            }
+        } 
     }
 }
