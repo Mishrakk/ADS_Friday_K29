478a09c641bb6166f66c40e81618edb0cf45e286
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-17 11:42:11 +0200

    Some UI

--- CoinChange/CoinChange/Program.cs
+++ CoinChange/CoinChange/Program.cs
@@ -6,6 +6,13 @@ namespace CoinChange
     {
         static void Main(string[] args)
         {
+            int[] Coins = { 1, 2, 5, 10, 20, 50 };
+            int change = 47;
+            Console.WriteLine("Hello, this is coin change calculator");
+            Console.WriteLine("Available coins: {0}", string.Join(", ", Coins));
+            Console.WriteLine("Change to be made: {0}", change);
+            
+            
         }
     }
 }
